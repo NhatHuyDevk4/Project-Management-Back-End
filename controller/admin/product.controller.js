dd0521024557c7ad80d25ac5b65e85a12e0054da
Le-Nhat-Huy-Qe180061 <lenhathuy9a6@gmail.com>
2024-12-23 22:11:11 +0700

    fix code

--- controller/admin/product.controller.js
+++ controller/admin/product.controller.js
@@ -1,35 +1,45 @@
 const Product = require("../../models/product.model");
 
-
-// [GET] /admin/products
+// [GET] /admin/products/
 module.exports.index = async (req, res) => {
-
-    let find = {
-        deleted: false,
-    }
-
-    // lọc theo trạng thái 
-    if(req.query.status){
-        find.status = req.query.status;
-    }
-    // End lọc trạng thái
-
-    // tìm kiếm
-    if(req.query.keyword){
-       //RegExp dùng để tìm kiếm chuỗi rồi trả về một đối tượng RegExp mới
-        const regex = new RegExp(req.query.keyword, "i"); // i là không phân biệt hoa thường
-        find.title = regex; // title là tên của trường cần tìm của đối tượng sản phẩm
-    }
-
-    const products = await Product.find(find);
-
-
-    res.render("admin/pages/products/index", {
-      pageTitle: "Trang sản phẩm",
-      products: products,
-    });
+  const find = {
+    deleted: false,
   };
-  
-  
-  
-  
+
+  const filterStatus = [
+    {
+      label: "Tất cả",
+      value: "",
+    },
+    {
+      label: "Hoạt động",
+      value: "active",
+    },
+    {
+      label: "Dừng hoạt động",
+      value: "inactive",
+    },
+  ];
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  // Tìm kiếm
+  let keyword = "";
+  if (req.query.keyword) {
+    const regex = new RegExp(req.query.keyword, "i");
+    find.title = regex;
+    keyword = req.query.keyword;
+  }
+  // Hết Tìm kiếm
+
+  const products = await Product.find(find);
+
+  res.render("admin/pages/products/index", {
+    pageTitle: "Quản lý sản phẩm",
+    products: products,
+    keyword: keyword,
+    filterStatus: filterStatus,
+  });
+};
