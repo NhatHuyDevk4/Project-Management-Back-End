af994cc52e760a5bea67e2e40f84b87df796d07b
Le-Nhat-Huy-Qe180061 <lenhathuy9a6@gmail.com>
2024-12-26 23:48:39 +0700

    Add change position of products

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -5,7 +5,7 @@ module.exports.index = async (req, res) => {
   const products = await Product.find({
     status: "active",
     deleted: false,
-  });
+  }).sort({ position: "desc" });
 
   const newProdcut = products.map((item) => {
     item.priceNew = (
