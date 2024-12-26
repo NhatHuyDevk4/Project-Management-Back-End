7af8f60606ccc4c7b43b002859938eb4b589be62
Le-Nhat-Huy-Qe180061 <lenhathuy9a6@gmail.com>
2024-12-26 22:55:18 +0700

    Add delete by who...

--- models/product.model.js
+++ models/product.model.js
@@ -10,6 +10,7 @@ const productSchema = new mongoose.Schema({
   status: String,
   position: Number,
   deleted: Boolean,
+  deltetedAt: Date,
 });
 
 const Product = mongoose.model("Product", productSchema, "products");
