f661fe2cc12a3fdde2bb28ef434d8caf6adced10
Le-Nhat-Huy-Qe180061 <lenhathuy9a6@gmail.com>
2024-12-30 22:11:40 +0700

    Add page detail product client

--- router/client/product.route.js
+++ router/client/product.route.js
@@ -5,4 +5,6 @@ const controller = require("../../controller/client/product.controller");
 
 router.get("/", controller.index);
 
+router.get("/detail/:slug", controller.slug);
+
 module.exports = router;
